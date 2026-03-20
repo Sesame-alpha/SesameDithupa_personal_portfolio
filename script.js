654b29fc959b40cdf9a74056adfd08c94d6a2f92
SESAME DITHUPA <bida25-018@thuto.bac.ac.bw>
2026-03-20 11:26:13 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,13 @@ function welcomeMessage() {
 }
  
 function openCert() {
-    window.open("https://your-link.com", "_blank");
+    window.open("https://coursera.org/share/bad1de5b695ff556808be1a85b949e6b", "_blank");
+     
+function openCert() {
+    window.open("https://coursera.org/share/13ef11ac93fd934ac3b487fdd392a281", "_blank");
+    function openCert() {
+    window.open("https://coursera.org/share/112d9df986e0107bf92cc07772b214bf", "_blank");
+    
 }
  
 /* Form validation */
